ffa387abd93aab8e71df771866a894fdf102303c
Maxwell Flood <maxwellflood@Maxwells-MBP-2.home>
2018-11-26 22:03:01 +0000

    Header animation added

--- dist/app.js
+++ dist/app.js
@@ -16,10 +16,47 @@
 
 // })
 
+var g1 = document.querySelector('.g-part-1')
+var g2 = document.querySelector('.g-part-2')
+var g3 = document.querySelector('.g-part-3')
+var g4 = document.querySelector('.g-part-4')
+var g5 = document.querySelector('.g-part-5')
+var logo = document.querySelector('.logo')
+var section01 = document.querySelector('.section-01')
+
+
+
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelectorAll('.int-01 div').forEach(div =>{
-        div.style.transform ="rotate(" + Math.round(Math.random()*100) + "deg)"
-    })
-    console.log('👋')
+    setTimeout(function(){
+        g1.style.width ="100%"
+    }, 1000)
+    setTimeout(function(){
+        g2.style.height ="100%"
+    }, 1300)
+    setTimeout(function(){
+        g3.style.width ="100%"
+    }, 1600)
+    setTimeout(function(){
+        g4.style.height ="50%"
+    }, 1900)
+    setTimeout(function(){
+        g5.style.width ="33%"
+    }, 2200)
+    setTimeout(function(){
+        logo.classList.remove("logoAnimation");
+    }, 2300)
+
 })
+
+// document.addEventListener('DOMContentLoaded', function(){
+//     document.querySelectorAll('.int-01 div').forEach(div =>{
+//         div.style.transform ="rotate(" + Math.round(Math.random()*100) + "deg)"
+//     })
+    
+//     setTimeout(function(){
+//         document.querySelector('.section-01', function(){
+//             this.style.height="0vh"
+//         })
+//     }, 3000)
+// })
 
