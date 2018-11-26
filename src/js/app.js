a309b1038781c4b83395616fcdb1eaa8acfe6c3c
Maxwell Flood <maxwellflood@Maxwells-MacBook-Pro-2.local>
2018-11-26 18:32:00 +0000

    dev branch

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,25 @@
 
-document.addEventListener('mousemove', function(e){
-    const x = e.pageX
-    const y = e.pageY
+// document.addEventListener('mousemove', function(e){
+//     const x = e.pageX
+//     const y = e.pageY
 
-    document.querySelectorAll('section div').forEach(div =>{
-        const dx = div.offsetLeft + 50 - x
-        const dy = div.offsetTop + 50 - y
-        const dist = Math.sqrt(dx*dx + dy*dy)
-        const score = Math.exp(dist*-0.003)
+//     document.querySelectorAll('section div').forEach(div =>{
+//         const dx = div.offsetLeft + 50 - x
+//         const dy = div.offsetTop + 50 - y
+//         const dist = Math.sqrt(dx*dx + dy*dy)
+//         const score = Math.exp(dist*-0.003)
 
-        //div.innerHTML = score.toFixed(2)
-        div.style.transform ="scale(" + score + ")" + " rotate(" + 0 + "deg)"
-        div.style.opacity = score
+//         //div.innerHTML = score.toFixed(2)
+//         div.style.transform ="scale(" + score + ")" + " rotate(" + 0 + "deg)"
+//         div.style.opacity = score
+//     })
+
+// })
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelectorAll('.int-01 div').forEach(div =>{
+        div.style.transform ="rotate(" + Math.round(Math.random()*100) + "deg)"
     })
-
+    console.log('👋')
 })
+
